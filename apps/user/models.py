fba4e37fb0e3df013c84209fb3cd6a404be33211
aliaryu <aliaryu@yahoo.com>
2024-10-11 20:00:53 +0330

    feat(user, models) : create user manager
    handle create user & create super user methods

--- apps/user/models.py
+++ apps/user/models.py
@@ -1,7 +1,44 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
-from apps.core.models import SoftDeleteModel
+from django.contrib.auth.models import (
+    AbstractUser,
+    BaseUserManager,
+)
+from apps.core.models import (
+    SoftDeleteModel,
+    SoftDeleteManager,
+)
+
+
+class UserManager(SoftDeleteManager, BaseUserManager):
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError(_("The Email must be set"))
+        if not password:
+            raise ValueError(_("The Password must be set"))
+
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        extra_fields.setdefault("is_active", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(email, password, **extra_fields)
 
 
 class User(SoftDeleteModel, AbstractUser):
@@ -24,6 +61,7 @@ class User(SoftDeleteModel, AbstractUser):
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
+    objects = UserManager()
 
     class Meta:
         verbose_name = _("user")
