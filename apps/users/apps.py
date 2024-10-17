82a8db72ae0f21bdf99e8e649625fecbb0130bde
aliaryu <aliaryu@yahoo.com>
2024-10-17 23:32:55 +0330

    feat(users, signals) : create user profile signal
    also config it into app module

--- apps/users/apps.py
+++ apps/users/apps.py
@@ -6,3 +6,6 @@ class UserConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.users'
     verbose_name = "app users"
+
+    def ready(self):
+        from . import signals
