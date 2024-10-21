502978f6bd9fdfca6a067780d0cb6b3e0a7a630b
aliaryu <aliaryu@yahoo.com>
2024-10-21 20:21:40 +0330

    chore(config, urls) : add media urls to url patterns

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from config import settings
+from django.conf.urls.static import static
 from debug_toolbar.toolbar import debug_toolbar_urls
 
 
@@ -8,6 +9,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/", include("apps.api.urls")),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     urlpatterns += debug_toolbar_urls()
