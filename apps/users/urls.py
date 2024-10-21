647fea1e039a9c2e5f0bb21c6546e119bd6697f0
aliaryu <aliaryu@yahoo.com>
2024-10-22 00:01:21 +0330

    chore(users, urls) : add user followers url

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from .api.views import (
-    UserListView
+    UserListView,
+    UserFollowersView
 )
 
 
 
 urlpatterns = [
-    path("user/", UserListView.as_view(), name="user-list")
+    path("user/", UserListView.as_view(), name="user-list"),
+
+    path("user/<int:pk>/followers/", UserFollowersView.as_view(), name="user-followers"),
     
 ]
