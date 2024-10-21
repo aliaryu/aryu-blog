0a9d259159ff029a7c446e2c1a1cc319a3872987
aliaryu <aliaryu@yahoo.com>
2024-10-22 01:30:29 +0330

    feat(users, api/serializers) : create user detail serializer

--- apps/users/api/serializers.py
+++ apps/users/api/serializers.py
@@ -19,7 +19,23 @@ class UserListSerializer(serializers.ModelSerializer):
         fields = ["id", "email", "first_name", "last_name",]
 
 
+class UserDetailSerializer(serializers.ModelSerializer):
+    profile = ProfileSerializer()
 
+    # this fields need annotate in related view
+    followers_count = serializers.IntegerField(read_only=True)
+    followings_count = serializers.IntegerField(read_only=True)
+
+    followers_url = serializers.HyperlinkedIdentityField(view_name="user-followers", lookup_field="pk")
+    followings_url = serializers.HyperlinkedIdentityField(view_name="user-followings", lookup_field="pk")
+
+    class Meta:
+        model = User
+        fields = [
+            "id", "email", "first_name", "last_name", "profile",
+            "followers_count", "followings_count",
+            "followers_url", "followings_url",
+        ]
 
 
 class UserFollowSerializer(serializers.ModelSerializer):
