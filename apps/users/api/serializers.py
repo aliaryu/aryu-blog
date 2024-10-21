ec459f597c0d3257b2e3cf44228df2622af2e719
aliaryu <aliaryu@yahoo.com>
2024-10-21 18:55:36 +0330

    feat(users, api/serializers) : create profile serializer
    also add profile serializer to user serializer as a field

--- apps/users/api/serializers.py
+++ apps/users/api/serializers.py
@@ -2,10 +2,19 @@ from django.utils.translation import gettext_lazy as _
 from rest_framework import serializers
 from ..models import (
     User,
+    Profile,
 )
 
 
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ["id", "user", "biography", "gender", "birth_date", "phone_number", "image", "profile_view"]
+
+
 class UserSerializer(serializers.ModelSerializer):
+    profile = ProfileSerializer()
+
     class Meta:
         model = User
-        fields = ["id", "email", "first_name", "last_name", ]
+        fields = ["id", "email", "first_name", "last_name", "profile"]
