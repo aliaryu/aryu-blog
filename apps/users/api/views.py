772a191c57f1dce748e5a8cd7e898b559c4d1453
aliaryu <aliaryu@yahoo.com>
2024-10-22 01:29:29 +0330

    refactor(users, api/serializers & views) : change user list serializer

--- apps/users/api/views.py
+++ apps/users/api/views.py
@@ -1,20 +1,23 @@
 from rest_framework import generics
 from .serializers import (
-    UserSerializer,
+    UserListSerializer,
     UserFollowSerializer,
+    UserDetailSerializer,
 )
 from ..models import User
 from django.db.models import Count
 
 
 class UserListView(generics.ListAPIView):
-    serializer_class = UserSerializer
+    serializer_class = UserListSerializer
 
     def get_queryset(self):
         return User.objects.all().select_related("profile").annotate(
             followers_count = Count("followers", distinct=True),
             followings_count = Count("followings", distinct=True)
         )
+
+
 
 
 class UserFollowersView(generics.ListAPIView):
