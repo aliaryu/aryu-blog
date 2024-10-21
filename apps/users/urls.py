62bcaf8eb86017065f1e4f479744aa7173c749df
aliaryu <aliaryu@yahoo.com>
2024-10-22 01:31:35 +0330

    chore(users, urls) : add user detail view url

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -3,12 +3,14 @@ from .api.views import (
     UserListView,
     UserFollowersView,
     UserFollowingsView,
+    UserDetailView,
 )
 
 
 
 urlpatterns = [
     path("", UserListView.as_view(), name="user-list"),
+    path("<int:pk>/", UserDetailView.as_view(), name="user-detail"),
 
     # followers/followings urls
     path("<int:pk>/followers/", UserFollowersView.as_view(), name="user-followers"),
