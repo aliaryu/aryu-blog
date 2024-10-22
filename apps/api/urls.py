da2910b89a184d0f95edca50aae7c58869267173
aliaryu <aliaryu@yahoo.com>
2024-10-22 22:42:53 +0330

    chore(api, urls) : add api root view url

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
+from .api.views import APIRootView
 
 
 urlpatterns = [
+    path("", APIRootView.as_view(), name="api-root"),
+
     path('api-auth/', include('rest_framework.urls')),
     
-    path("users/", include("apps.users.urls"))
+    path("users/", include("apps.users.urls")),
 ]
