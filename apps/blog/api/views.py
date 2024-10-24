bd1417cf076ee3e421f13b340010e26fa03df8d6
aliaryu <aliaryu@yahoo.com>
2024-10-24 22:13:16 +0330

    feat(blog, api/views) : create post list view

---
+++ apps/blog/api/views.py
@@ -0,0 +1,19 @@
+from rest_framework import generics
+from rest_framework import views
+from ..models import (
+    Tag,
+    Post,
+)
+from .serializers import (
+    PostSerializer,
+)
+
+
+
+
+
+class PostListView(generics.ListAPIView):
+    serializer_class = PostSerializer
+
+    def get_queryset(self):
+        return Post.objects.all()
