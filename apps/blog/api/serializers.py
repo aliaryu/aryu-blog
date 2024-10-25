35cc47c34a0a3fded691573ccee4f981d62852ce
aliaryu <aliaryu@yahoo.com>
2024-10-25 18:25:04 +0330

    feat(blog, api/serializers) : add likes count field to post serializer

--- apps/blog/api/serializers.py
+++ apps/blog/api/serializers.py
@@ -17,11 +17,12 @@ class PostSerializer(serializers.ModelSerializer):
     author_detail = serializers.HyperlinkedRelatedField(view_name="users:user-detail", read_only=True, source="author")
     author_email = serializers.EmailField(source="author.email", read_only=True)
     content = serializers.SerializerMethodField()
+    likes_count = serializers.IntegerField(read_only=True)
 
     class Meta:
         model = Post
         fields = [
-            "id" , "author_detail", "author_email", "title", "content", "post_view",
+            "id" , "author_detail", "author_email", "title", "content", "post_view", "likes_count",
         ]
 
     def get_content(self, obj):
