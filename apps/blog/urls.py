4f88b990749f9196177a5be42406246440050f9f
aliaryu <aliaryu@yahoo.com>
2024-10-26 17:46:42 +0330

    chore(blog, urls) : add post detail url

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .api.views import (
     PostListView,
+    PostDetailView,
 )
 
 
 app_name = "blog"
 urlpatterns = [
     path("posts/", PostListView.as_view(), name="post-list"),
+    path("posts/<slug:slug>/", PostDetailView.as_view(), name="post-detail"),
 ]
