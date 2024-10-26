993835405dfc959ee1db199973e0cec9eee6203e
aliaryu <aliaryu@yahoo.com>
2024-10-26 22:07:57 +0330

    chore(blog, urls) : add post comment url

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .api.views import (
     PostListView,
     PostDetailView,
+    PostCommentsView
 )
 
 
@@ -9,4 +10,8 @@ app_name = "blog"
 urlpatterns = [
     path("posts/", PostListView.as_view(), name="post-list"),
     path("posts/<slug:slug>/", PostDetailView.as_view(), name="post-detail"),
+
+    # post comments
+    path("posts/<slug:slug>/comments/", PostCommentsView.as_view(), name="post-comments"),
+
 ]
