5ca476eac180e327560241933a9566ddab23476d
aliaryu <aliaryu@yahoo.com>
2024-10-29 04:27:14 +0330

    feat(blog, api/serializers) : customize update method post detail serializer

--- apps/blog/api/serializers.py
+++ apps/blog/api/serializers.py
@@ -33,24 +33,42 @@ class PostListSerializer(serializers.ModelSerializer):
 
 class PostDetailSerializer(serializers.ModelSerializer):
     tags = serializers.SerializerMethodField()
+    update_tags = serializers.ListField(
+        child = serializers.CharField(),
+        write_only = True,
+        required = False
+    )
     author_detail = serializers.HyperlinkedRelatedField(view_name="users:user-detail", read_only=True, source="author")
     author_email = serializers.EmailField(source="author.email", read_only=True)
     likes_count = serializers.IntegerField(read_only=True)
     comments = serializers.HyperlinkedIdentityField(view_name="blog:post-comments", lookup_field="slug")
-    
-    class Meta:
-        model = Post
-        fields = [
-            "id" , "author_detail", "author_email", "title", "content", "create_at", "update_at",
-            "allow_comments", "post_view", "likes_count", "tags", "comments"
-        ]
-        read_only_fields = ["create_at", "update_at", "post_view"]
 
     def get_tags(self, obj):
         return [tag.tag_name for tag in obj.tags.all()]
 
-    
+    class Meta:
+        model = Post
+        fields = [
+            "id" , "author_detail", "author_email", "title", "content", "create_at", "update_at",
+            "allow_comments", "post_view", "likes_count", "tags", "update_tags", "comments",
+        ]
+        read_only_fields = ["create_at", "update_at", "post_view"]
 
+    def update(self, instance, validated_data):
+        tags_list = validated_data.pop("update_tags", [])
+        instance.title = validated_data.get("title", instance.title)
+        instance.content = validated_data.get("content", instance.content)
+        instance.allow_comments = validated_data.get("allow_comments", instance.allow_comments)
+        instance.save()
+
+        if tags_list:
+            existing_tags = set(Tag.objects.filter(tag_name__in=tags_list).values_list("tag_name", flat=True))
+            new_tags = [Tag(tag_name=tag_name) for tag_name in tags_list if tag_name not in existing_tags and tag_name]
+            if new_tags:
+                Tag.objects.bulk_create(new_tags)
+            all_tags = Tag.objects.filter(tag_name__in=tags_list)
+            instance.tags.set(all_tags)
+        return instance
 
 
 class PostCreateSerializer(serializers.ModelSerializer):
