351e9a008e83cf3762c2c487e5fa416df3e510b8
aliaryu <aliaryu@yahoo.com>
2024-11-01 14:07:02 +0330

    feat(users, api/serializers) : create user register serializer

--- apps/users/api/serializers.py
+++ apps/users/api/serializers.py
@@ -61,3 +61,25 @@ class UserFollowSerializer(serializers.ModelSerializer):
         extra_kwargs = {
             "url": {"view_name": "users:user-detail"},
         }
+
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    confirm_password = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ["email", "first_name", "last_name", "password", "confirm_password"]
+        extra_kwargs = {
+            "password": {"write_only": True},
+            "confirm_password": {"write_only": True},
+        }
+
+    def validate(self, data):
+        if data["password"] != data["confirm_password"]:
+            raise serializers.ValidationError(_("passwords do not match"))
+        return data
+
+    def create(self, validated_data):
+        validated_data.pop("confirm_password")  
+        user = User.objects.create_user(**validated_data)
+        return user
