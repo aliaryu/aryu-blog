08b442dfeed1d0e243d033c7ddf3e4578e2ab77d
aliaryu <aliaryu@yahoo.com>
2024-11-01 18:41:23 +0330

    feat(users, utils) : create send activation email function using django default token generator

---
+++ apps/users/utils.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes
+from django.core.mail import send_mail
+from rest_framework.reverse import reverse
+from django.conf import settings
+
+
+def send_activation_email(user, request):
+    token = default_token_generator.make_token(user)
+    uid = urlsafe_base64_encode(force_bytes(user.pk))
+    activation_link = request.build_absolute_uri(
+        reverse("users:user-activate", kwargs={"uidb64": uid, "token": token})
+    )
+    subject = "Activate your account"
+    message = f"Hello {user.username},\n\nPlease activate your account using the link below:\n{activation_link}"
+
+    send_mail(
+        subject,
+        message,
+        settings.DEFAULT_FROM_EMAIL,
+        [user.email],
+        fail_silently=False,
+    )
