54e25c683765c6141810dafc08b004776680c5ba
aliaryu <aliaryu@yahoo.com>
2024-11-03 00:12:38 +0330

    feat(core, permissions) : create is user owner post permissions

--- apps/core/permissions.py
+++ apps/core/permissions.py
@@ -1,6 +1,11 @@
 from rest_framework import permissions
 
 
+class IsUserOwnerPost(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.author == request.user
+
+
 class IsUserOwnerOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
