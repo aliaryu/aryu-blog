195b5ba268067759f7da086637c4b4659404b9a3
aliaryu <aliaryu@yahoo.com>
2024-11-03 18:23:49 +0330

    chore(api, api/views) : add drf spectacular urls to api root view

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -26,7 +26,6 @@ urlpatterns = [
 
 
     path("schema/", SpectacularAPIView.as_view(), name="schema"),
-
     path("schema/swagger-ui/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
     path("schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
 ]
