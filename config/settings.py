a5e1509f029882e10ca328d36dcc19236d6c20e3
aliaryu <aliaryu@yahoo.com>
2024-11-09 02:36:55 +0330

    feat(config, settings) : add environment for email backend

--- config/settings.py
+++ config/settings.py
@@ -162,14 +162,18 @@ else:
     CELERY_TIMEZONE = TIME_ZONE
 
     # EMAIL
-    EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-    EMAIL_HOST = config("EMAIL_HOST")
-    EMAIL_PORT = config("EMAIL_PORT")
-    EMAIL_HOST_USER = config("EMAIL_HOST_USER")
-    EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
-    EMAIL_USE_TLS = config("EMAIL_USE_TLS", cast=bool)
-    EMAIL_USE_SSL = config("EMAIL_USE_SSL", cast=bool)
-    DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL")
+    USE_REAL_EMAIL = config("USE_REAL_EMAIL", default=False, cast=bool)
+    if USE_REAL_EMAIL:
+        EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+        EMAIL_HOST = config("EMAIL_HOST")
+        EMAIL_PORT = config("EMAIL_PORT")
+        EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+        EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+        EMAIL_USE_TLS = config("EMAIL_USE_TLS", cast=bool)
+        EMAIL_USE_SSL = config("EMAIL_USE_SSL", cast=bool)
+        DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL")
+    else:
+        EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
