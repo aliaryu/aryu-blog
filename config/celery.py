aec20fdb3d41eb9f4c6b960d83f8ed4130284fb8
aliaryu <aliaryu@yahoo.com>
2024-11-08 21:04:42 +0330

    feat(config, celery) : create celery configs

---
+++ config/celery.py
@@ -0,0 +1,11 @@
+from django.conf import settings
+
+
+if not settings.DEBUG:
+    import os
+    from celery import Celery
+
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+    app = Celery("aryu_blog")
+    app.config_from_object("django.conf:settings", namespace="CELERY")
+    app.autodiscover_tasks()
