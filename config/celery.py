84061dea4cb610a3e4bdc687b08c3a7f9e060068
aliaryu <aliaryu@yahoo.com>
2024-11-08 22:05:24 +0330

    fix(config, celery) : change import order

--- config/celery.py
+++ config/celery.py
@@ -1,11 +1,11 @@
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 from django.conf import settings
 
 
 if not settings.DEBUG:
-    import os
     from celery import Celery
 
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
     app = Celery("aryu_blog")
     app.config_from_object("django.conf:settings", namespace="CELERY")
     app.autodiscover_tasks()
